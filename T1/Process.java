f2922dd663c23401659c3446b6619c96e1059d10
Lucca Carvalho <luccalafonte97@gmail.com>
2018-09-30 12:45:53 -0300

    Trata mensagem recebida

--- T1/Process.java
+++ T1/Process.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.LinkedList;
 
 class Process {
 	static private int clock;
@@ -8,12 +9,14 @@ class Process {
 	static private int basePort;
 	static private Socket clientSocket;
 	static private Socket connectionSocket;
+	static private LinkedList<Message> msgList;
 
 	public static void main(String argv[]) throws IOException{
 		clock = Integer.parseInt(argv[0]);
 		pid = Integer.parseInt(argv[1]);
 		quant = Integer.parseInt(argv[2]);
 		basePort = 7000;
+		msgList = new LinkedList<Message>();
 		new Thread(enviaMensagem).start();
 		new Thread(recebeMensagem).start();
 	}
@@ -26,7 +29,7 @@ class Process {
 					StringBuilder message = new StringBuilder();
 					message.append("0"+'\n'+Integer.toString(clock)+Integer.toString(pid)+'\n'+data.readLine()+'\n');
 					for(int i=0; i<quant; i++){
-						clientSocket = new Socket("200.18.101.31", basePort+i);
+						clientSocket = new Socket("192.168.0.12", basePort+i);
 						DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 						outToServer.writeBytes(message.toString());
 						clientSocket.close();
@@ -61,15 +64,39 @@ class Process {
 			try{
 				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 
-				//DataOutputStream outToClient = new DataOutputStream(this.connectionSocket.getOutputStream());
+				//DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-				Message m = new Message(inFromCLient)
+				boolean isAck = false;
 
-				String aux = inFromClient.readLine();
-				while(aux!=null){
-					System.out.println(aux);
-					aux = inFromClient.readLine();
+				Message rcvMsg = null;
+
+				if(inFromClient.readLine().equals("1")){
+					isAck = true;
 				}
+
+				else{
+					rcvMsg = new Message(inFromClient);
+					clock = Math.max(rcvMsg.getClock(), clock) + 1;
+				}
+
+				if(isAck){
+					System.out.println("Recebi ACK no tempo " + clock);
+				}
+				else{
+					int i=0;
+					while(i<msgList.size() && msgList.get(i).getGlobalClock()<rcvMsg.getGlobalClock())
+						i++;
+					msgList.add(i, rcvMsg);
+					StringBuilder sndMessage = new StringBuilder();
+					sndMessage.append("1"+'\n'+Integer.toString(clock)+'\n'+Integer.toString(rcvMsg.getGlobalClock()));
+					for(i=0; i<quant; i++){
+						clientSocket = new Socket("192.168.0.12", basePort+i);
+						DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+						outToServer.writeBytes(sndMessage.toString());
+						clientSocket.close();
+					}
+				}
+
 			}
 			catch(Exception e){
 				e.printStackTrace();
